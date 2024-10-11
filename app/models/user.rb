102addcedd78335ee4e4c7cf45b2e11a7ad7dacf
Dovran Atayev <d.atayev@sumatosoft.com>
2024-10-11 13:20:53 +0300

    add post and subsription model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts, dependent: :destroy
+
+  has_many :subscriptions, foreign_key: :follower_id, class_name: 'Subscription'
+  has_many :followings, through: :subscriptions, source: :following
+
+  has_many :subscribers, foreign_key: :following_id, class_name: 'Subscription'
+  has_many :followers, through: :subscribers, source: :follower
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_11_091800) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_11_095540) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.bigint "follower_id", null: false
+    t.bigint "following_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follower_id", "following_id"], name: "index_subscriptions_on_follower_id_and_following_id", unique: true
+    t.index ["follower_id"], name: "index_subscriptions_on_follower_id"
+    t.index ["following_id"], name: "index_subscriptions_on_following_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +44,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_11_091800) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
+  add_foreign_key "subscriptions", "users", column: "follower_id"
+  add_foreign_key "subscriptions", "users", column: "following_id"
 end
